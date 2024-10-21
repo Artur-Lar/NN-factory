18224cffec6379871b2eda66c73a87537a82504c
1988Artur <larionov.artur@mail.ru>
2024-10-21 15:00:13 +0300

    added mobile version

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ window.addEventListener("wheel", (event) => {
     }
   }
 });
+
+let startY = 0;
+let endY = 0;
+
+window.addEventListener("touchstart", (event) => {
+  startY = event.touches[0].clientY;
+});
+
+window.addEventListener("touchmove", (event) => {
+  endY = event.touches[0].clientY;
+});
+
+window.addEventListener("touchend", () => {
+  if (startY > endY + 50) {
+    if (currentSection < sections.length - 1) {
+      currentSection++;
+      scrollToSection(currentSection);
+    }
+  } else if (startY < endY - 50) {
+    if (currentSection > 0) {
+      currentSection--;
+      scrollToSection(currentSection);
+    }
+  }
+});
+
+const burgerMenu = document.getElementById("burger-menu");
+const mobileNav = document.getElementById("mobile-nav");
+
+burgerMenu.addEventListener("click", () => {
+  mobileNav.style.display =
+    mobileNav.style.display === "block" ? "none" : "block";
+});
